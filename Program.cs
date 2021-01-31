81d5f2b43b6e9377ff661a0042e486f179a24bee
Venkata Sai Raghavendra Rao Sunkavalli <sunkavalli2@usf.edu>
2021-01-31 16:49:50 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace diffpair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 2, 5, 6, 7 };
+            Console.WriteLine("Q4: Enter the absolute difference to check");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] - arr[j] == k || arr[j] - arr[i] == k)
+                    {
+                        count++;
+                    }
+
+                }
+            }
+
+
+            Console.WriteLine(count);
+        }
+    }
+}
